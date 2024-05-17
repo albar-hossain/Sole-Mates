fe1807e2ade8b17bf1041631f8851af9934a5921
Albar Hossain <albarhossain@gmail.com>
2024-05-17 20:06:15 +0600

    updates

---
+++ Views/catalog.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Sole Mates Website</title>
+    <link rel ="stylesheet" type="text/css" href="style.css">
+    <script src="https://kit.fontawesome.com/92d70a2fd8.js"></script>
+
+
+</head>
+<body>
+
+    
+    <form>
+
+        <div class="nav">
+
+
+        
+            <div class="logo">Sole<span class="mates">Mate</span></div>
+           <a href="home.php">Home</a>
+            <a href="">Men</a>
+            <a href="">Women</a>
+            <a href="">Children</a>
+            <a href="">Accessories</a>
+             <div class="nav-icons">
+        <a href=""> <i class="fa-solid fa-magnifying-glass"></i></a>
+        
+                <a href=""><i class="fa-solid fa-user"></i></a>
+            
+            <a href=""><i class="fa-solid fa-cart-shopping"></i></a>
+            </div>
+
+
+            
+        </div>
+
+        
+        </div>
+
+        <footer class="footer">
+        <div class="container10">
+            <div class="row">
+                <div class="footer-col">
+                <h4>Service</h4>
+                <ul>
+                    <li><a href="#">Men's Shoes</a></li>
+                    <li><a href="#">Women's Shoes</a></li>
+                    <li><a href="#">Kid's Shoes</a></li>
+                    <li><a href="#">Accessories</a></li>
+                </ul>
+            </div>
+            <div class="footer-col">
+                <h4>Support</h4>
+                <ul>
+                    <li><a href="#">Pricing Plan</a></li>
+                    <li><a href="#">Documentation</a></li>
+                    <li><a href="#">Guide</a></li>
+
+                </ul>
+            </div>
+            <div class="footer-col">
+                <h4>company</h4>
+                <ul>
+                    <li><a href="#">About</a></li>
+                    <li><a href="#">Press</a></li>
+                    <li><a href="#">Partner</a></li>
+                </ul>
+            </div>
+
+            <div class="footer-col">
+                <h4>Legal</h4>
+                <ul>
+                    <li><a href="#">Claim</a></li>
+                    <li><a href="#">Privacy</a></li>
+                    <li><a href="#">Terms</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+
+        
+    </footer>
+
---
+++ Views/details.php
@@ -0,0 +1,111 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Sole Mates Website</title>
+    <link rel="stylesheet" type="text/css" href="../Assets/css/style.css">
+    <script src="https://kit.fontawesome.com/92d70a2fd8.js"></script>
+</head>
+<body>
+    <form>
+        <div class="nav">
+            <div class="logo">Sole<span class="mates">Mate</span></div>
+            <a href="..\home.php">Home</a>
+            <a href="">Men</a>
+            <a href="">Women</a>
+            <a href="">Children</a>
+            <a href="">Accessories</a>
+            <div class="nav-icons">
+                <a href=""> <i class="fa-solid fa-magnifying-glass"></i></a>
+                <a href=""><i class="fa-solid fa-user"></i></a>
+                <a href=""><i class="fa-solid fa-cart-shopping"></i></a>
+            </div>
+        </div>
+
+        <div class="Detailbox">
+            <!-- Shoe Picture -->
+            <img src="../Assets/Images/orange.png" alt="Shoe Picture" width="500px">
+            <!-- Product Details -->
+            <div class="product-details">
+                <p><strong>Product details:</strong></p>
+                <p><strong>Material:</strong></p>
+                <ul>
+                    <li>UPPER: 52% Synthetic, 48% Textile</li>
+                    <li>OUTSOLE: Synthetic Rubber</li>
+                </ul>
+                <p><strong>Care instructions:</strong> Hand Wash</p>
+                <p><strong>Sole material:</strong> Synthetic Rubber</p>
+                <p><strong>Shaft height:</strong> Ankle</p>
+            </div>
+
+            <div class="additional-text">
+            <p><strong>NIKE SHOES 4</strong></p>
+            <p>“Midnight Navy”</p>
+            <p><strong>2,190 BDT</strong></p>
+            <p><strong>Promotion:</strong> 50% OFF</p>
+            <!-- Buttons -->
+            <button class="add-to-cart-btn">ADD TO CART</button>
+            <button class="buy-now-btn">BUY NOW</button><br>
+
+            <select class="size-selector">
+            <option value="6">Size 6</option>
+            <option value="7">Size 7</option>
+            <option value="8">Size 8</option>
+            <option value="9">Size 9</option>
+           
+        </select>
+        <!-- Quantity Selector -->
+        <input type="number" class="quantity-selector" value="1" min="1" max="10">
+    </div>
+        </div>
+        </div>
+
+        <footer class="footer">
+        <div class="container10">
+            <div class="row">
+                <div class="footer-col">
+                <h4>Service</h4>
+                <ul>
+                    <li><a href="#">Men's Shoes</a></li>
+                    <li><a href="#">Women's Shoes</a></li>
+                    <li><a href="#">Kid's Shoes</a></li>
+                    <li><a href="#">Accessories</a></li>
+                </ul>
+            </div>
+            <div class="footer-col">
+                <h4>Support</h4>
+                <ul>
+                    <li><a href="#">Pricing Plan</a></li>
+                    <li><a href="#">Documentation</a></li>
+                    <li><a href="#">Guide</a></li>
+
+                </ul>
+            </div>
+            <div class="footer-col">
+                <h4>company</h4>
+                <ul>
+                    <li><a href="#">About</a></li>
+                    <li><a href="#">Press</a></li>
+                    <li><a href="#">Partner</a></li>
+                </ul>
+            </div>
+
+            <div class="footer-col">
+                <h4>Legal</h4>
+                <ul>
+                    <li><a href="#">Claim</a></li>
+                    <li><a href="#">Privacy</a></li>
+                    <li><a href="#">Terms</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+
+        
+    </footer>
+
+
+    </form>
+</body>
+</html>
